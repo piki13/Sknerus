40fadff663ae26ee7f866747dbb982b8d1ff6972
piki13 <paulinakozuch@gmail.com>
2024-03-17 04:39:19 +0100

    Dodano historię wyszukiwań.

--- Skapiec/Controllers/HomeController.cs
+++ Skapiec/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 ﻿using System.Diagnostics;
 using Skapiec.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skapiec.Entities;
 
 namespace Skapiec.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SkapiecDBcontext dBcontext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SkapiecDBcontext dbContext, ILogger<HomeController> logger)
         {
+            dBcontext = dbContext;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var productsFromDb = await dBcontext.Products.ToListAsync();
+            ViewBag.Products = productsFromDb;
+            return View(); // Przekieruj do widoku QueryResults.cshtml
+        }
+        public async Task<IActionResult> ShowResults(string queryColumn)
+        {
+            var productsFromDb = await dBcontext.Products
+                                                .Where(p => p.query == queryColumn)
+                                                .ToListAsync();
+            ViewBag.Products = productsFromDb;
+            return View("Index", productsFromDb); // Przekieruj do widoku Index.cshtml z wynikami zapytania
         }
 
         public IActionResult Privacy()
