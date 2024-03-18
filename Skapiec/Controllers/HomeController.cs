21f476d37d0ae2746a9894680c9207aca3424ce0
piki13 <paulinakozuch@gmail.com>
2024-03-18 15:42:00 +0100

    Dodano sortowanie produktów w historii

--- Skapiec/Controllers/HomeController.cs
+++ Skapiec/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Skapiec.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Skapiec.Entities;
+using System.Linq;
 
 namespace Skapiec.Controllers
 {
@@ -17,8 +18,6 @@ namespace Skapiec.Controllers
             _logger = logger;
         }
 
-
-
         [HttpGet]
         public async Task<IActionResult> Index()
         {
@@ -28,24 +27,43 @@ namespace Skapiec.Controllers
             return View(); // Przekieruj do widoku QueryResults.cshtml
         }
 
-
         [HttpPost]
-        public async Task<IActionResult> Index(string queryColumn)
+        public async Task<IActionResult> Index(string queryColumn, string sortOrder)
         {
             var AllproductsFromDb = await dBcontext.Products.ToListAsync();
             var productsFromDb = await dBcontext.Products
                                                 .Where(p => p.query == queryColumn)
                                                 .ToListAsync();
 
-            //var productsFromDb = await dBcontext.Products.ToListAsync();
+            // Sortowanie
+            switch (sortOrder)
+            {
+                case "NameDesc":
+                    productsFromDb = productsFromDb.OrderByDescending(p => p.Name).ToList();
+                    break;
+                case "PriceAsc":
+                    productsFromDb = productsFromDb.OrderBy(p => p.Value).ToList();
+                    break;
+                case "PriceDesc":
+                    productsFromDb = productsFromDb.OrderByDescending(p => p.Value).ToList();
+                    break;
+                case "DateAsc":
+                    productsFromDb = productsFromDb.OrderBy(p => p.CreationTime).ToList();
+                    break;
+                case "DateDesc":
+                    productsFromDb = productsFromDb.OrderByDescending(p => p.CreationTime).ToList();
+                    break;
+                default:
+                    
+                    productsFromDb = productsFromDb.OrderBy(p => p.Name).ToList();
+                    break;
+            }
 
             ViewBag.Products = AllproductsFromDb;
             ViewBag.Selected = productsFromDb;
+            ViewBag.SortOrder = sortOrder; // Przekazanie informacji o aktualnym sposobie sortowania do widoku
             return View("Index", productsFromDb); // Przekieruj do widoku Index.cshtml z wynikami zapytania
         }
-
-
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
